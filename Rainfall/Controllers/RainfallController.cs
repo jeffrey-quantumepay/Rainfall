341ea46c2c2d106a359d1b43d4126da4d3bca9de
Jeffrey <jeff@e2payments.com>
2024-03-02 16:56:00 +0800

    Add project files.

---
+++ Rainfall/Controllers/RainfallController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Rainfall.Web.API.Controllers
+{
+    [Route("")]
+    public class RainfallController : ControllerBase
+    {
+
+        public RainfallController()
+        {
+        }
+
+        [HttpGet("/rainfall/id/{stationId}/readings")]
+        public async Task<IActionResult> Readings([FromRoute] string stationId, [FromQuery] string count)
+        {
+            return  Ok(new { stationId, count });
+
+        }
+    }
+}
