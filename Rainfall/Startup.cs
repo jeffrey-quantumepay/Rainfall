caccef3b65bf32112da9fe91274d90159e0e71b5
Jeffrey <jeff@e2payments.com>
2024-03-06 21:07:37 +0800

    update swagger doc

--- Rainfall/Startup.cs
+++ Rainfall/Startup.cs
@@ -4,6 +4,7 @@ using FluentValidation.AspNetCore;
 using MediatR;
 using System.Reflection;
 using Rainfall.Application;
+using Microsoft.OpenApi.Models;
 
 namespace Rainfall.Web.API
 {
@@ -37,20 +38,42 @@ namespace Rainfall.Web.API
                 // options
             })
               .AddJsonOptions(options => {
-                  // options
-                  //options.JsonSerializerOptions.
-
               });
+
+
+
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Rainfall API",
+                    Version = "1.0",
+                    Description = "An API which provides rainfall reading data",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Sorted",
+                        Url = new Uri("https://wwww.sorted.com")
+                    },
+                    
+                });
+
+                options.AddServer(new OpenApiServer()
+                {
+                    Url = "http://localhost:3000",
+                    Description = "Rainfall Api"
+                });
+
+                // https://stackoverflow.com/questions/62424769/using-swashbuckle-5-x-specify-nullable-true-on-a-generic-t-parameter-reference
+                options.UseAllOfToExtendReferenceSchemas();
+
+                foreach (var file in Directory.GetFiles(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)), "*.xml"))
+                {
+                    options.IncludeXmlComments(file, true);
+                }
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseSwagger();
-            }
-            app.UseSwaggerUI();
-
 
             app.UseHttpsRedirection();
 
@@ -61,6 +84,31 @@ namespace Rainfall.Web.API
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseSwaggerUI(options => {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Rainfall API");
+                });
+
+                app.UseReDoc(options => {
+                    options.DocumentTitle = "Rainfall API";
+                    options.SpecUrl = "/swagger/v1/swagger.json";
+                });
+            }
+            else
+            {
+                app.UseSwaggerUI(options => {
+                    options.SwaggerEndpoint("/rainfall/swagger/v1/swagger.json", "Rainfall API");
+                });
+
+                app.UseReDoc(options => {
+                    options.DocumentTitle = "Rainfall API";
+                });
+            }
         }
      }
 }
